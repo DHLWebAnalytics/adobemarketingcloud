d10a833dba2bf5239059a01a0130d97dccc5890a
Fooku18 <jakob.schlonsok@gmail.com>
2020-10-12 20:53:34 +0200

    increased version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="experiencecloudapis",
-    version="1.1.0",
+    version="1.2.0",
     author="DHL Web Analytics",
     author_email="analytics@dpdhl.com",
     license='MIT',
